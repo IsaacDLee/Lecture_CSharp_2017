20233c0c741d3ae84e7962c085edaeecb71c2156
isaac <isaac@kog.co.kr>
2017-03-09 19:05:20 +0900

    3, 5

---
+++ Quiz/Quiz/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiz
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int sum = 0;
+
+            for (int i = 1; i <= 10000; i++)
+            {
+                if ((i % 3 == 0) || (i % 5 == 0) )
+                    sum += i;
+
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
